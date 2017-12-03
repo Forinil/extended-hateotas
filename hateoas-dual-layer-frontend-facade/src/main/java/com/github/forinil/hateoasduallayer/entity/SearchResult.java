701bf5e47b03a4c9624ed470d66ca8327c295903
Konrad Botor <kbotor@gmail.com>
2017-12-03 17:05:03 +0100

    :new: Added creation date to SearchResult entity

--- hateoas-dual-layer-frontend-facade/src/main/java/com/github/forinil/hateoasduallayer/entity/SearchResult.java
+++ hateoas-dual-layer-frontend-facade/src/main/java/com/github/forinil/hateoasduallayer/entity/SearchResult.java
@@ -1,17 +1,20 @@
 package com.github.forinil.hateoasduallayer.entity;
 
 import com.github.forinil.hateoasduallayer.model.ApplicationData;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.gemfire.mapping.annotation.Region;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Region("SearchCache")
 @ToString
+//@AllArgsConstructor(onConstructor_={@PersistenceConstructor})
 public class SearchResult {
 
     @Id
@@ -19,12 +22,21 @@ public class SearchResult {
     private final UUID id;
 
     @Getter
+    private LocalDate creationDate;
+
+    @Getter
     private List<ApplicationData> applicationList;
 
     @PersistenceConstructor
-    public SearchResult(UUID id, List<ApplicationData> applicationList) {
+    public SearchResult(UUID id, LocalDate creationDate, List<ApplicationData> applicationList) {
         this.id = id;
+        this.creationDate = creationDate;
         this.applicationList = applicationList;
     }
 
+    public SearchResult(UUID id, List<ApplicationData> applicationList) {
+        this.id = id;
+        this.creationDate = LocalDate.now();
+        this.applicationList = applicationList;
+    }
 }
