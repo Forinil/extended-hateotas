7629d25a8f4b1139b14c7211992e0f4467c7f447
Konrad Botor <kbotor@gmail.com>
2017-12-22 17:24:52 +0100

    :new: Added default Spring properties support to custom H2 Console configuration, added H2 Console connection list configuration

--- hateoas-dual-layer-common/hateoas-dual-layer-common-profiles/src/main/java/com/github/forinil/hateoasduallayer/profile/H2Database.java
+++ hateoas-dual-layer-common/hateoas-dual-layer-common-profiles/src/main/java/com/github/forinil/hateoasduallayer/profile/H2Database.java
@@ -2,6 +2,6 @@ package com.github.forinil.hateoasduallayer.profile;
 
 import org.springframework.context.annotation.Profile;
 
-@Profile({"jpa_data", "h2"})
+@Profile({"h2"})
 public @interface H2Database {
 }
